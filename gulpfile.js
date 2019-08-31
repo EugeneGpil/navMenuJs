3cfd62a5d7c0c79f5e6d51ec68d270b5d6eb7ca0
EugeneGpil <gpilligrim@gmail.com>
2019-08-31 09:09:12 +0300

    'header_nav'

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// modules
+const gulp = require('gulp');
+const cleanCss = require('gulp-clean-css');
+const sass = require('gulp-sass');
+
+//paths
+const sassSrc = 'src/sass/style.scss';
+const cssDest = 'public/css'
+
+
+
+
+gulp.task('css', () => {
+    return gulp.src(sassSrc)
+        .pipe(sass())
+        .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch', function (){
+    gulp.watch(sassSrc, gulp.series('css'));
+});
+
+
+
+
+gulp.task('production', () => {
+    gulp.src('node_modules/normalize.css/normalize.css')
+        .pipe(cleanCss())
+        .pipe(gulp.dest(cssDest))
+
+    gulp.src('node_modules/animate.css/animate.min.css')
+        .pipe(gulp.dest(cssDest));
+
+    return gulp.src(sassSrc)
+        .pipe(sass())
+        .pipe(cleanCss())
+        .pipe(gulp.dest(cssDest));
+});
