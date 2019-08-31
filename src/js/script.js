c041f90f713dbe901c1184fda06bbd70584d23ec
EugeneGpil <gpilligrim@gmail.com>
2019-08-31 13:33:01 +0300

    'mibile_menu_complete'

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 "use strict";
 
 var navMobileButton = document.getElementById("mobile-nav-button");
+var navMobile = document.getElementById("nav-mobile");
+
+navMobileButton.addEventListener('click', function () {
+    navMobile.classList.remove("hidden");
+    navMobile.classList.toggle("bounceInLeft");
+    navMobile.classList.toggle("bounceOutLeft");
+});
