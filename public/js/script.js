e51f52ee1fa11c85e44f0ec4bae3bae3536bffb7
EugeneGpil <gpilligrim@gmail.com>
2019-09-04 10:56:12 +0300

    'nav_plus_submenu'

--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,26 @@
 var navMobileButton = document.getElementById("mobile-nav-button");
 var navMobile = document.getElementById("nav-mobile");
 
-navMobileButton.addEventListener('click', function () {
-    navMobile.classList.remove("hidden");
-    navMobile.classList.toggle("bounceInLeft");
-    navMobile.classList.toggle("bounceOutLeft");
+function toggle(element) {
+    element.classList.remove("hidden");
+    element.classList.toggle("bounceInLeft");
+    element.classList.toggle("bounceOutLeft");
+}
+
+navMobileButton.addEventListener('click', function () { toggle(navMobile) });
+
+
+
+var secondMenuElement = document.getElementById("second-menu-element");
+var submenu = document.getElementById("submenu");
+
+secondMenuElement.addEventListener('click', function () { toggle(submenu) });
+
+
+
+var navMobileElementWithSubmenu = document.getElementById("nav-mobile__element-with-submenu");
+var navMobileElementSubmenuContainer = document.getElementById("nav-mobile__element-submenu-container");
+
+navMobileElementWithSubmenu.addEventListener('click', function () {
+    navMobileElementSubmenuContainer.classList.toggle("hidden");
 });
